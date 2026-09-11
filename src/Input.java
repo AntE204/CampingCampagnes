3c3df0092d1f0f1dd4fb1a11312ccc77bf446f43
AntE204 <anthonysimonbtssio2@gmail.com>
2026-09-11 15:26:34 +0200

    Modif noms fonctions class Input

--- src/Input.java
+++ src/Input.java
@@ -4,34 +4,34 @@ import java.util.Scanner;
 public class Input {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static String ask_string(String message) {
+    public static String get_string(String message) {
         IO.print(message);
         return Input.scanner.nextLine();
     }
 
-    public static int ask_int(String message) {
+    public static int get_int(String message) {
         IO.print(message);
 
         try {
             return Input.scanner.nextInt();
         }
         catch (Exception ex) {
-            return Input.ask_int(message);
+            return Input.get_int(message);
         }
     }
 
-    public static double ask_double(String message) {
+    public static double get_double(String message) {
         IO.print(message);
 
         try {
-            // On prend l'input en string, on forcel e caractère décimal en point, puis on cast la valeur en double
+            // On prend l'input en string, on force le caractère décimal en point, puis on cast la valeur en double
             String input_str = Input.scanner.nextLine();
             DecimalFormatSymbols dfs = new DecimalFormatSymbols();
             input_str = input_str.replace(dfs.getDecimalSeparator(), '.');
             return Double.parseDouble(input_str);
         }
         catch (Exception ex) {
-            return Input.ask_double(message);
+            return Input.get_double(message);
         }
     }
 
