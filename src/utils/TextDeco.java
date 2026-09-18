74035f5c3b6605818e4db9559b24cda6d792b297
AntE204 <anthonysimonbtssio2@gmail.com>
2026-09-18 21:52:40 +0200

    Ajout colorisation texte
    Ajout d'une classe qui permet d'afficher du texte en couleur dans la console grâce aux codes d'échappement ANSI.

---
+++ src/utils/TextDeco.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class TextDeco {
+    public static final String
+    RESET  = "\u001B[0m",
+
+    RED = "\u001B[38;2;255;63;63m",
+    GREEN = "\u001B[38;2;63;255;63m",
+    BLUE = "\u001B[38;2;0;155;255m",
+    GREY = "\u001B[38;2;128;128;128m",
+
+    HIGHLIGHT_BLUE = "\u001B[48;2;0;155;255m",
+    HIGHLIGHT_WHITE = "\u001B[48;2;0;155;255m";
+}
