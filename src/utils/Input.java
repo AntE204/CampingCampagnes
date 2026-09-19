23428fea345e9e8ef18cdadd8d53763b6ea28c83
AntE204 <anthonysimonbtssio2@gmail.com>
2026-09-19 16:44:24 +0200

    Corrige stack overflow dans demande d'input
    Les fonctions de la classe Input se réappelaient elles-mêmes si l'input était invalide, ce qui remplissait la stack. Le problème a été corrigé en utilisant une boucle qui continue tant que l'input n'est pas valide.

--- src/utils/Input.java
+++ src/utils/Input.java
@@ -11,48 +11,48 @@ public class Input {
     }
 
     public static int get_int(String message) {
-        IO.print(message);
-
-        try {
-            return Input.scanner.nextInt();
-        }
-        catch (Exception ex) {
-            return Input.get_int(message);
+        while (true) {
+            IO.print(message);
+            try {
+                return Input.scanner.nextInt();
+            }
+            catch (Exception ex) {
+                // L'exception est ignorée
+            }
         }
     }
 
     public static double get_double(String message) {
-        IO.print(message);
-
-        try {
-            // On prend l'input en string, on force le caractère décimal en point, puis on cast la valeur en double
-            String input_str = Input.scanner.nextLine();
-            DecimalFormatSymbols dfs = new DecimalFormatSymbols();
-            input_str = input_str.replace(dfs.getDecimalSeparator(), '.');
-            return Double.parseDouble(input_str);
-        }
-        catch (Exception ex) {
-            return Input.get_double(message);
+        while (true) {
+            IO.print(message);
+            try {
+                // On prend l'input en string, on force le caractère décimal en point, puis on cast la valeur en double
+                String input_str = Input.scanner.nextLine();
+                DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+                input_str = input_str.replace(dfs.getDecimalSeparator(), '.');
+                return Double.parseDouble(input_str);
+            }
+            catch (Exception ex) {
+                // L'exception est ignorée
+            }
         }
     }
 
     public static boolean get_bool(String message, char yes, char no) {
-        IO.print(message);
+        while (true) {
+            IO.print(message);
+            try {
+                char input = Input.scanner.nextLine().charAt(0);
 
-        try {
-            char input = Input.scanner.nextLine().charAt(0);
+                if (input == yes)
+                    return true;
 
-            if (input == yes)
-                return true;
-
-            else if (input == no)
-                return false;
-
-            else
-                return Input.get_bool(message, yes, no);
-        }
-        catch (Exception ex) {
-            return Input.get_bool(message, yes, no);
+                else if (input == no)
+                    return false;
+            }
+            catch (Exception ex) {
+                // L'exception est ignorée
+            }
         }
     }
 }
